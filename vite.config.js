c90e503b7b78f1241945d5369a59b06213f8748d
Prajyusha Rath <prajyusha@Prajyushas-MacBook-Air.local>
2025-11-19 00:01:25 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // crucial for drag-and-drop deploy
+  base: "./" // crucial for GitHub Pages
 });
